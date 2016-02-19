f5e2b6727b8fcc6f4c25861e98bd49a1a7ebc102
aikolesnikov <artyom.kolesnikov@gmail.com>
2016-02-19 22:38:40 +0200

    Create dz3.cpp

---
+++ dz3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "dz3.h"
+
+using namespace std;
+
+int main()
+{
+//    dz3_1();
+    dz3_2();
+//    dz3_3();
+//    dz3_4();
+//    dz3_5();
+//    dz3_6();
+//    dz3_7();
+}
