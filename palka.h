672fa533ed94704e56470e20dd7e60340a2338b2
aikolesnikov <artyom.kolesnikov@gmail.com>
2016-02-24 20:28:35 +0200

    Update palka.h

--- palka.h
+++ palka.h
@@ -3,68 +3,23 @@
 
 using namespace std;
 
-
-//------------------------------------------------------------------------
-void DrawFigure(int a, int b, int x, int y, char draw_char){
-
-    // positioning to x,y
-    for (int i=0; i<x; ++i)
-        cout << endl;
-    // for (int i=0; i<y; ++i)
-    //    cout << " ";
-
-    // drawing figure
-    for (int i=0; i<b; ++i){
-        for (int j=0; j<y; ++j)
-            cout << " ";
-        for (int j=0; j<a; ++j)
-            cout << draw_char;
-        cout << endl;
-    }
+void gotoxy(int x, int y)
+{
+COORD coord;
+coord.X = x;
+coord.Y = y;
+SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
-//------------------------------------------------------------------------
-
-void ClearArea(int a, int b, int x, int y, char draw_char){
-
-    DrawFigure(a, b, x, y, draw_char);
-
-}
 
 //------------------------------------------------------------------------
+void DrawFigure(int width, int hight, int x, int y, char draw_char){
 
-
-int MoveFigure(){
-
-    int Top=10, Left=10;  // start position
-    int a=5, b=7;       // size of figure a*b
-
-    while (Left<80){  // 80-L
-
-        system("cls");
-
-        // positioning to start
-        for (int i=0; i<Top; ++i){
-            cout << endl;
-        }
-        for (int i=0; i<a; ++i){
-            for (int j=0; j<Left; ++j){
-                cout << " ";
-            }
-            for (int j=0; j<b; ++j){
-                cout << "*";
-            }
-            cout << endl;
-        }
-        // drawing
-
-        Left+=1;
-        Top+=1;
-        Sleep(100);
-
+    for (int i=0; i<hight; ++i){
+            gotoxy(x,y+i);
+        for (int j=0; j<width; ++j)
+                cout << draw_char;
     }
-
-return 0;
 }
 
 #endif // PALKA_H_INCLUDED
