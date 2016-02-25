f9ee77de21d9b7a7ec46fd81b9c82063a770d273
aikolesnikov <artyom.kolesnikov@gmail.com>
2016-02-25 21:54:59 +0200

    Create dz4.cpp

---
+++ dz4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "dz4.h"
+using namespace std;
+
+double factorial(int x);
+
+
+int main()
+{
+
+
+/* Факториал
+int n;
+cout << "Input n for factorial: "; cin >> n;
+cout << "n=" << n << "; Factorial " << n << "! = " << factorial(n) << endl;
+*/
+    return 0;
+}
