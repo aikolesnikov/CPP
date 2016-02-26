4966e3a5b8be60f5ad6547320729c15ea17df9e9
aikolesnikov <artyom.kolesnikov@gmail.com>
2016-02-26 19:11:45 +0200

    Create cpp_core_1.h

---
+++ cpp_core_1.h
@@ -0,0 +1,25 @@
+#ifndef CPP_CORE_1_INCLUDED
+#define CPP_CORE_1_INCLUDED
+
+using namespace std;
+
+void gotoxy(int x, int y)
+{
+COORD coord;
+coord.X = x;
+coord.Y = y;
+SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+
+//------------------------------------------------------------------------
+void DrawFigure(int width, int hight, int x, int y, char draw_char){
+
+    for (int i=0; i<hight; ++i){
+            gotoxy(x,y+i);
+        for (int j=0; j<width; ++j)
+                cout << draw_char;
+    }
+}
+
+#endif // CPP_CORE_1_INCLUDED
